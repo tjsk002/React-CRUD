4063f56dedc9302a210c957724e8d45051c72df4
tjsk002 <tjsk002@naver.com>
2025-04-04 16:40:45 +0900

    feat: 인터셉터 리프레시 토큰 발급 API 연동 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,3 +7,43 @@ export const api = axios.create({
     },
     withCredentials: true,
 })
+
+const refreshAccessToken = async () => {
+    await api
+        .post(
+            '/auth/refresh',
+            {},
+            {
+                headers: {
+                    authorization: localStorage.getItem('accessToken'),
+                },
+            }
+        )
+        .then((response) => {
+            const newAccessToken = response.headers.authorization
+            if (!newAccessToken) {
+                alert('로그아웃 되었습니다. 다시 로그인해주세요.')
+                window.location.href = '/auth/login'
+            }
+
+            if (newAccessToken) {
+                localStorage.clear()
+                localStorage.setItem('accessToken', newAccessToken)
+            }
+        })
+        .catch((error) => {
+            console.log(`error ${error}`)
+        })
+}
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+        if (error.response?.status === 403 && !originalRequest._retry) {
+            originalRequest._retry = true
+            await refreshAccessToken()
+        }
+        return Promise.reject(error)
+    }
+)
