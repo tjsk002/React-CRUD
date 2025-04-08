8420f7a946e06ad73de280fcd706f87cb5ae5c86
tjsk002 <tjsk002@naver.com>
2025-04-08 09:53:31 +0900

    feat: 로그인 axios 분리, react-query 적용, zod 사용

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
 import { api } from '@/api/axios.ts'
-import { AdminInfo } from '@/pages/auth/schema/auth-info-schema.tsx'
+import { AdminInfo, LoginInfo } from '@/pages/auth/schema/auth-info-schema.tsx'
 
 const ENDPOINT_AUTH = '/auth'
 const ROLE_ADMIN = 'ADMIN'
@@ -12,4 +12,11 @@ export const createProcess = async (data: AdminInfo) => {
         password: data.password,
     })
     return response.data.resultData
+}
+
+export const loginProcess = async (data: LoginInfo) => {
+    return await api.post(`${ENDPOINT_AUTH}/login`, {
+        username: data.username,
+        password: data.password,
+    })
 }
