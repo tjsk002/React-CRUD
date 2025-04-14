ec863d611621e6595acb98b94961cb9015cd55b8
tjsk002 <tjsk002@naver.com>
2025-04-14 16:56:06 +0900

    feat: 회원 로그인 추가

--- src/pages/auth/schema/auth-info-schema.tsx
+++ src/pages/auth/schema/auth-info-schema.tsx
@@ -37,3 +37,18 @@ export const authInfoSchema = z.object({
 })
 
 export type AuthInfo = z.infer<typeof authInfoSchema>
+
+export const loginInfoSchema = z.object({
+    username: z
+        .string()
+        .min(1, { message: '아이디을 입력해주세요.' })
+        .min(5, { message: '5~12자리여야 합니다.' })
+        .max(12, { message: '5~12자리여야 합니다.' }),
+    password: z
+        .string()
+        .min(1, { message: '비밀번호를 입력해주세요.' })
+        .min(5, { message: '5~12자리여야 합니다.' })
+        .max(12, { message: '5~12자리여야 합니다.' }),
+})
+
+export type LoginInfo = z.infer<typeof loginInfoSchema>
