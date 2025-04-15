15c144f03b0859821f69c6cad3709411de769891
tjsk002 <tjsk002@naver.com>
2025-04-15 13:21:23 +0900

    feat: header 로그인 했을 시 로그인 하지 않았을 시 view 처리

--- src/pages/common/header.tsx
+++ src/pages/common/header.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 
 export default function Header() {
     const navigate = useNavigate()
+    const [user, setUser] = useState({
+        username: '',
+        nickName: '',
+    })
+    const [isOpen, setIsOpen] = useState(false)
+    const fetchMy = () => {
+        const stored = localStorage.getItem('userData')
+        if (!stored) return
+
+        try {
+            const userData = JSON.parse(stored)
+            if (userData && userData.username) {
+                setUser({
+                    username: userData.username,
+                    nickName: userData.nickName,
+                })
+            }
+        } catch (error) {
+            console.error('userData 파싱 실패:', error)
+        }
+    }
 
     function viewLogin() {
         navigate('/auth/login')
@@ -15,6 +37,10 @@ export default function Header() {
         navigate('/')
     }
 
+    useEffect(() => {
+        fetchMy()
+    })
+
     return (
         <div>
             <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-200 shadow-sm h-14 flex items-center justify-between px-40">
@@ -23,20 +49,42 @@ export default function Header() {
                         🎬 MovieYou
                     </a>
                 </h1>
-                <div className="space-x-3 text-sm">
-                    <button
-                        className="px-3 py-1 text-gray-700 hover:text-blue-600"
-                        onClick={viewLogin}
-                    >
-                        로그인
-                    </button>
-                    <button
-                        className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 shadow"
-                        onClick={viewSignup}
-                    >
-                        회원가입
-                    </button>
-                </div>
+                {!user && (
+                    <div className="space-x-3 text-sm">
+                        <button
+                            className="px-3 py-1 text-gray-700 hover:text-blue-600"
+                            onClick={viewLogin}
+                        >
+                            로그인
+                        </button>
+                        <button
+                            className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 shadow"
+                            onClick={viewSignup}
+                        >
+                            회원가입
+                        </button>
+                    </div>
+                )}
+                {user && (
+                    <div className="relative text-sm">
+                        <div
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="text-gray-800 w-40 py-3 text-sm rounded-md hover:bg-gray-300 transition text-center cursor-default"
+                        >
+                            {user.nickName}님
+                        </div>
+                        {isOpen && (
+                            <div className="absolute right-0 mt-2 w-40 bg-white text-black rounded-md shadow-md border border-gray-200">
+                                <button className="block w-40 text-center px-5 py-3 text-sm hover:bg-gray-100 transition">
+                                    내 정보
+                                </button>
+                                <button className="block w-40 text-center px-5 py-3 text-sm hover:bg-gray-100 transition">
+                                    로그아웃
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                )}
             </header>
         </div>
     )
