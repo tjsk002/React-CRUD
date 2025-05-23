e9e9b3264f16c7548408957b33084f37786fdc24
tjsk002 <tjsk002@naver.com>
2025-05-23 17:18:27 +0900

    chore: footer 내용 수정

--- src/pages/common/footer.tsx
+++ src/pages/common/footer.tsx
@@ -9,7 +9,8 @@ export default function Footer() {
     return (
         <footer className="bg-white text-center text-sm text-gray-500 py-4 mt-10">
             <div className="flex flex-col items-center gap-1">
-                <p>© 2025 무비유. All rights reserved.</p>
+                회사소개 | 제휴안내 | 광고안내 | 이용약관 | 개인정보처리방침 | 청소년보호정책
+                <p>© 2025 FeedSpot. All rights reserved.</p>
                 <a onClick={handleAdmin} className="text-blue-500 hover:underline cursor-default">
                     관리자 페이지
                 </a>
