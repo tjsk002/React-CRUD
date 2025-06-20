fb4d5c25b94298b51c20c36cf7e5afdeeb3feee1
tjsk002 <tjsk002@naver.com>
2025-06-20 15:31:07 +0900

    feat: index 페이지 공지사항 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { getBoards } from '@/api/boarad.ts'
 import Footer from '@/pages/common/footer.tsx'
 import Header from '@/pages/common/header.tsx'
 import BoardList from '@/pages/service/board/list.tsx'
+import NoticeList from '@/pages/service/notice.tsx'
 import { useQuery } from '@tanstack/react-query'
 
 export default function Index() {
@@ -17,6 +18,10 @@ export default function Index() {
                 <section className="mb-12 w-2/3 pr-8">
                     <h1 className="text-xl font-semibold text-gray-800 mb-8">인기 게시물</h1>
                     <BoardList boardList={data?.list} />
+                </section>
+                <section className="mb-12 w-1/3 pr-8">
+                    <h1 className="text-xl font-semibold text-gray-800 mb-8">공지 사항</h1>
+                    <NoticeList></NoticeList>
                 </section>
             </main>
             <Footer />
--- src/pages/service/notice.tsx
+++ src/pages/service/notice.tsx
@@ -1,6 +1,4 @@
 import { getNotices } from '@/api/notices.ts'
-import Footer from '@/pages/common/footer.tsx'
-import Header from '@/pages/common/header.tsx'
 import { useQuery } from '@tanstack/react-query'
 import { Eye } from 'lucide-react'
 
@@ -12,41 +10,32 @@ type noticeListInfo = {
     updatedAt: string
 }
 
-export default function Notice() {
+export default function NoticeList() {
     const { data } = useQuery({
         queryKey: ['notices'],
         queryFn: () => getNotices(),
     })
     return (
-        <div className="min-h-screen bg-gray-50 flex flex-col">
-            <Header />
-            <main className="pt-28 px-40 flex-grow">
-                <h1 className="text-xl font-bold text-gray-800 mb-8">공지사항</h1>
-                <ul className="divide-y divide-gray-200 bg-white shadow-sm rounded-lg overflow-hidden">
-                    {data?.map((notice: noticeListInfo) => (
-                        <li className="px-6 py-4 hover:bg-gray-100 cursor-pointer transition-colors">
-                            <div className="flex justify-between items-center">
-                                <div className="flex items-center space-x-2 max-w-[70%]">
-                                    <span className="text-sm text-gray-900 truncate">
-                                        {notice.title}
-                                    </span>
-                                    <span className="flex items-center space-x-1 text-sm text-gray-500">
-                                        <Eye size={16} />
-                                        <span>{notice.viewCount}</span>
-                                    </span>
-                                </div>
-                                <div className="flex items-center space-x-6 text-sm text-gray-500 whitespace-nowrap">
-                                    <span>
-                                        {new Date(notice.createdAt).toLocaleDateString()}
-                                        {new Date(notice.createdAt).toLocaleTimeString()}
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-            <Footer />
-        </div>
+        <ul className="divide-y divide-gray-200 bg-white shadow-sm rounded-lg overflow-hidden">
+            {data?.map((notice: noticeListInfo) => (
+                <li className="px-6 py-4 hover:bg-gray-100 cursor-pointer transition-colors">
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center space-x-2 max-w-[70%]">
+                            <span className="text-sm text-gray-900 truncate">{notice.title}</span>
+                            <span className="flex items-center space-x-1 text-sm text-gray-500">
+                                <Eye size={16} />
+                                <span>{notice.viewCount}</span>
+                            </span>
+                        </div>
+                        <div className="flex items-center space-x-6 text-sm text-gray-500 whitespace-nowrap">
+                            <span>
+                                {new Date(notice.createdAt).toLocaleDateString()}
+                                {new Date(notice.createdAt).toLocaleTimeString()}
+                            </span>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
     )
 }
