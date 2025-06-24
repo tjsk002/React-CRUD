27258be1b008a3b4b83e1421904dfbb202bbdbbb
tjsk002 <tjsk002@naver.com>
2025-06-24 09:39:01 +0900

    feat: 공시사항 조회수 API 연동

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -11,3 +11,10 @@ export const getNoticeDetail = async (noticeId: number) => {
     const response = await api.get(`${ENDPOINT_NOTICES}/${noticeId}`)
     return response.data.resultData.data
 }
+
+export const increaseViewCount = async (noticeId: number) => {
+    const response = await api.post(`${ENDPOINT_NOTICES}/view-count`, {
+        noticeId: noticeId,
+    })
+    return response.data.resultData.data
+}
